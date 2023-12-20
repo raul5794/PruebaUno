a7e4dcc894eab9d22db5023d6aba36b5f5217d2c
raul <raul@gmail.com>
2023-12-19 20:30:31 -0500

    estructura

---
+++ src/main/java/com/example/azureDeveops/AzureDeveopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.azureDeveops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDeveopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDeveopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azureDeveops/AzureDeveopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azureDeveops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDeveopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
